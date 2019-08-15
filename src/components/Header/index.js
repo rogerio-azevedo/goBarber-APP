58ee40fda00ae07312a6b915fd94a07f534c7ff8
Rogerio Azevedo <rogerioazevedo@pop-os.localdomain>
2019-08-14 20:55:26 -0400

    Stilo Notifications

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Notifications from '~/components/Notifications';
 import logo from '~/assets/logo-purple.svg';
 
 import { Container, Content, Profile } from './styles';
@@ -14,6 +15,8 @@ export default function Header() {
         </nav>
 
         <aside>
+          <Notifications />
+
           <Profile>
             <div>
               <strong>Rogerio Azevedo</strong>
